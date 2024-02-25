867aefca85389b56066ae928384d791c386acb9b
Arda-Kurtulus <161217346+Arda-Kurtulus@users.noreply.github.com>
2024-02-25 16:31:30 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+meme_dict = {
+            "CRINGE": "Garip ya da utandırıcı bir şey",
+            "LOL": "Komik bir şeye verilen cevap",
+            }
+    #values
+for i in meme_dict.keys():
+    #print(meme_dict[i]) 
+    print(i)
